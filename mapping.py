d15f7dd25b32dc6bb31b99116470655a43da1d88
Nate Olson <nolson@nist.gov>
2019-03-14 10:47:18 -0400

    added bam file checks

--- mapping.py
+++ mapping.py
@@ -35,6 +35,15 @@ def run_mapping(fastq, out_bam, read_group_args, genome_path, threads=1):
     index_command = f"samtools index {out_bam}"
     run_command(index_command)
 
+    ## BAM file sanity checks ####################
+    ## Checking file - mapping stats and for incomplete file
+    idxstat_command = f"samtools idxstat {out_bam}"
+    run_command(idxstat_command)
+
+    ## Checking header format
+    header_command = f"samtools view -h {out_bam}"
+    run_command(header_command) 
+
 
 def merge_bams(combined_path, bams, genome_path):
     # merge_command = f"samtools merge -f -O cram --reference {genome_path} {combined_path} {' '.join(bams)}"
